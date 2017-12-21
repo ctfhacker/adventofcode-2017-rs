e0e686207593867603e5d069830278df934f74f5
thebarbershopper <cld251@gmail.com>
2017-12-20 21:43:36 -0600

    Comments on day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -6,18 +6,18 @@ fn main() {
     // let mut f = File::open("./test").expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something wrong happened reading file");
-    let result: u32 = contents.chars()
-                              .enumerate()
+    let result: u32 = contents.chars()                                                  // Read the file contents by characters
+                              .enumerate()                                              // Get an iterator of (index, char)
                               .filter(|&(i, c)| {
-                                  if i == (contents.chars().count()-1) { 
+                                  if i == (contents.chars().count()-1) {                
                                       println!("{}", i);
-                                      return c == contents.chars().nth(0).unwrap();
+                                      return c == contents.chars().nth(0).unwrap();     // For the last character, compare it to the first (circular)
                                   } else {
-                                      return c == contents.chars().nth(i+1).unwrap();
+                                      return c == contents.chars().nth(i+1).unwrap();   // Compare the current character to the next character
                                   }
                               })
-                              .map(|(i, c)| c.to_digit(10).unwrap())
-                              .sum();
+                              .map(|(i, c)| c.to_digit(10).unwrap())                    // Convert all found characters to digits
+                              .sum();                                                   // Sum all found digits
 
     println!("Result: {:?}", result);
 }

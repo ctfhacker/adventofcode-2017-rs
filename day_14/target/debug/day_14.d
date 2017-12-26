5a61f4ae20b454f1d372692cefbe45a45ba805f8
thebarbershopper <cld251@gmail.com>
2017-12-26 11:47:12 -0600

    Day 14 completed

--- day_14/target/debug/day_14.d
+++
@@ -1,1 +0,0 @@
-/home/ctfhacker/workspace/adventofcode-2017-rs/day_14/target/debug/day_14: /home/ctfhacker/workspace/adventofcode-2017-rs/day_14/src/main.rs
--- day_14/target/debug/deps/day_14-1b8998acad4cc172.d
+++
@@ -1,5 +0,0 @@
-/home/ctfhacker/workspace/adventofcode-2017-rs/day_14/target/debug/deps/day_14-1b8998acad4cc172: src/main.rs
-
-/home/ctfhacker/workspace/adventofcode-2017-rs/day_14/target/debug/deps/day_14-1b8998acad4cc172.d: src/main.rs
-
-src/main.rs:
